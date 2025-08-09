28d9699365879187a58b4e26413391b0a3a252da
Cx330-Paulownia <Cx330t_Personal@163.com>
2025-08-09 14:15:16 +0800

    Create initial commit with project structure

---
+++ src/config.ts
@@ -0,0 +1,100 @@
+import type {
+	ExpressiveCodeConfig,
+	CommentConfig,
+	LicenseConfig,
+	NavBarConfig,
+	ProfileConfig,
+	SiteConfig,
+	UmamiConfig,
+} from "./types/config";
+import { LinkPreset } from "./types/config";
+
+export const siteConfig: SiteConfig = {
+	title: "桐の小窝",
+	subtitle: "这有只泡桐酱~",
+	lang: "zh_CN", // Language code, e.g. 'en', 'zh-CN', 'ja', etc.
+	themeColor: {
+		hue: 300, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		fixed: false, // Hide the theme color picker for visitors
+	},
+	banner: {
+		enable: false,
+		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+		credit: {
+			enable: false, // Display the credit text of the banner image
+			text: "", // Credit text to be displayed
+			url: "", // (Optional) URL link to the original artwork or artist's page
+		},
+	},
+	toc: {
+		enable: true, // Display the table of contents on the right side of the post
+		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
+	},
+	favicon: [
+		 {
+		   src: '/favicon/Cx330t.png',    // Path of the favicon, relative to the /public directory
+		 //  theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+		 //  sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		 }
+	],
+};
+
+export const navBarConfig: NavBarConfig = {
+	links: [
+		LinkPreset.Home,
+		LinkPreset.Archive,
+		LinkPreset.About,
+		LinkPreset.Friends,
+	],
+};
+
+export const profileConfig: ProfileConfig = {
+	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "Lorem Ipsum",
+	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	links: [
+		{
+			name: "Twitter",
+			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+			// You will need to install the corresponding icon set if it's not already included
+			// `pnpm add @iconify-json/<icon-set-name>`
+			url: "https://x.com/Cx330_Paulownia",
+		},
+		{
+			name: "GitHub",
+			icon: "fa6-brands:github",
+			url: "https://github.com/Cx330-Paulownia",
+		},
+		{
+			name: "bilibili",
+			icon: "fa6-brands:bilibili",
+			url: "https://space.bilibili.com/3493088685198058",
+		},
+	],
+};
+
+export const licenseConfig: LicenseConfig = {
+	enable: true,
+	name: "CC BY-NC-SA 4.0",
+	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+};
+
+export const expressiveCodeConfig: ExpressiveCodeConfig = {
+	// Note: Some styles (such as background color) are being overridden, see the astro.config.mjs file.
+	// Please select a dark theme, as this blog theme currently only supports dark background color
+	theme: "github-dark",
+};
+
+export const commentConfig: CommentConfig = {
+  twikoo: {
+    envId: 'https://twikoo.cx330t.online/.netlify/functions/twikoo',
+  },
+}
+
+export const umamiConfig: UmamiConfig = {
+	enable: true,
+	baseUrl: "umami.cx330t.online",
+	shareId: "XGvmlGf6tnhcVBEn",
+	timezone: "Asia/Shanghai",
+};
---
+++ src/content/spec/about.md
@@ -0,0 +1,63 @@
+# 关于
+
+> ## Vanitas Vanitatum Et Omnia Vanitas
+>
+
+本站由 [Astro](https://astro.build/)&[Fuwari](https://github.com/saicaca/fuwari)强力驱动
+::github{repo="withastro/astro"}
+
+::github{repo="saicaca/fuwari"}
+
+> ## 其他支持
+
+评论系统 [Twikoo](https://github.com/twikoojs/twikoo)
+::github{repo="twikoojs/twikoo"}
+统计系统 [Umami](https://github.com/umami-software/umami)
+::github{repo="umami-software/umami"}
+本站已全面接入 [EdgeOne](https://edgeone.ai)
+::github{repo="TencentEdgeOne/edgefunctions-examples"}
+
+> ## 关于本站
+
+目前博客的方向有：
+
+- 日记向（既是记下学校生活，不至于成年后发现一片空白。~~也能练习markdown语法~~）
+
+- 暂定（会有搞机的文章？咕咕咕~）
+
+> ## 关于我
+
+:::tip
+嘛，仅供参考。即使这是我自己写的。
+:::
+
+是一个16岁的高中生，坐标位于安徽。
+
+目前是只小蝴蝶？但这目前大抵就是永久了。
+
+## 喜欢做的事？
+
+画自己想画的（二次元）
+
+做自己想做的（捣鼓博客、编译、看番、或者和游戏开发部一起玩游戏）
+
+~~还有睡觉!~~
+
+## “日常”？
+
+嘛，活着就行！（其实高中生活能不那么累更好）
+
+~~离自己家里远点~~（话这么说，但终归藕断丝连）
+
+<details>
+<summary>还有吗？</summary>
+唔...泡桐还没想好呢！
+</details>
+
+> ## 小窝の运营
+- 2025.5.10 开始研究，并选择了高性能的Astro作为博客的框架
+
+<details>
+<summary>更多</summary>
+咕咕咕~
+</details>
